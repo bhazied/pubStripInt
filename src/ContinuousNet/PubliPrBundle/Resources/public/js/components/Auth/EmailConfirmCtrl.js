785bd691ef8f80578b54ec86c524c4195549afef
Sahbi KHALFALLAH <sahbi.khalfallah@continuousnet.com>
2016-04-06 21:45:29 +0100

    change routing

--- src/ContinuousNet/PubliPrBundle/Resources/public/js/components/Auth/EmailConfirmCtrl.js
+++ src/ContinuousNet/PubliPrBundle/Resources/public/js/components/Auth/EmailConfirmCtrl.js
@@ -11,18 +11,20 @@ app.controller('EmailConfirmCtrl', ['$scope', '$rootScope', '$localStorage', '$s
         $scope.authenticated = false;
 
         if (angular.isDefined($stateParams.token)) {
-            $registerDataFactory.emailConfirm({locale: $localStorage.language, token: $stateParams.token}).$promise.then(function(data) {
-                $timeout(function(){
-                    if (data.status) {
-                        $scope.status = 'success';
-                        $localStorage.access_token = data.token;
-                        $scope.user = $localStorage.user = $rootScope.user = data.user;
-                        $scope.authenticated = true;
-                    } else {
-                        $scope.status = 'error';
-                    }
-                }, 2000);
-            });
+            $timeout(function() {
+                $registerDataFactory.emailConfirm({locale: $localStorage.language, token: $stateParams.token}).$promise.then(function(data) {
+                    $timeout(function(){
+                        if (data.status) {
+                            $scope.status = 'success';
+                            $localStorage.access_token = data.token;
+                            $scope.user = $localStorage.user = $rootScope.user = data.user;
+                            $scope.authenticated = true;
+                        } else {
+                            $scope.status = 'error';
+                        }
+                    }, 2000);
+                });
+            }, 2000)
         } else {
             $scope.status = 'warning';
         }
