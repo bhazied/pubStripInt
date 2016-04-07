1895206a2d8107ac7d9fb714bc1f669beb1e1fb4
Sahbi KHALFALLAH <sahbi.khalfallah@continuousnet.com>
2016-04-07 20:10:52 +0100

    all fixes

--- src/ContinuousNet/PubliPrBundle/Resources/public/js/components/Auth/LoginCtrl.js
+++ src/ContinuousNet/PubliPrBundle/Resources/public/js/components/Auth/LoginCtrl.js
@@ -7,24 +7,34 @@ app.controller('LoginCtrl', ['$scope', '$rootScope', '$localStorage', '$state', 
 function ($scope, $rootScope, $localStorage, $state, $timeout, $loginDataFactory) {
     
     if ($localStorage.access_token) {
-        //delete $localStorage.access_token;
+        if ($localStorage.rememberMe && $localStorage.rememberMe == true) {
+            $state.go('app.dashboard');
+        } else {
+            delete $localStorage.access_token;
+        }
     }
             
     $scope.status = '';
     $scope.user = {};
     $scope.disableSubmit = false;
+    $scope.rememberMe = false;
     
     $scope.submit = function () {
         $scope.disableSubmit = true;
 		$scope.user = {email: $scope.email, password: $scope.password};
         $loginDataFactory.check($scope.user).$promise.then(function(data) {
+            if ($scope.rememberMe) {
+                $localStorage.rememberMe = true;
+            } else {
+                $localStorage.rememberMe = false;
+            }
             $scope.disableSubmit = false;
             $scope.status = 'welcome';
             $localStorage.access_token = data.token;
             $scope.user = $localStorage.user = $rootScope.user = data.user;
-            $timeout(function() {
+            //$timeout(function() {
                 $state.go('app.dashboard');
-            }, 1000);
+            //}, 1000);
         }, function(error) {
             $scope.disableSubmit = false;
             $scope.status = 'error';
