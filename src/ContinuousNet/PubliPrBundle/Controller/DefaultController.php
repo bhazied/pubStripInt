ccc09ae2af8e713f7aa7cebd1b2055e977a3aa74
Zied Ben Hadj Amor <zied.benhadjamor@continuousnet.com>
2016-05-09 18:12:38 +0100

    ma première commit sur ma branche dev_zied

--- src/ContinuousNet/PubliPrBundle/Controller/DefaultController.php
+++ src/ContinuousNet/PubliPrBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace ContinuousNet\PubliPrBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+use Hip\MandrillBundle\Message;
+use Hip\MandrillBundle\Dispatcher;
 
 class DefaultController extends Controller
 {
@@ -14,5 +17,27 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+    }
+
+    /**
+     * @Route("/testMail")
+     */
+    public function testMailAction()
+    {
+
+        $dispatsher = $this->get('hip_mandrill.dispatcher');
+        $message = new Message();
+        $message
+            ->setFromName('zied.benhadjamor@continuousnet.com')
+            ->setFromName('Zied Ben Hadj Amor')
+            ->addTo('mr.bha.zied@gmail.com', 'Zied gmail')
+            ->addTo('zied.benhadjamor@itipart.com', 'zied itipart')
+            ->addTo('zied.benhadjamor@dotit.com.tn', 'zied dotit')
+            ->setSubject('some subject')
+            ->setHtml('<html><body>Zied est là et il test encore</body></html>');
+
+        $result = $dispatsher->send($message);
+        dump($result);
+        return new Response('<pre>' . print_r($result, true) . '</pre>');
     }
 }
