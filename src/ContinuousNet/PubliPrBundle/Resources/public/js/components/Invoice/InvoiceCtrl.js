e9ffcb3ed52738e267d4bfabf7aee544761e0060
Sahbi KHALFALLAH <sahbi.khalfallah@continuousnet.com>
2016-06-01 10:55:55 -0700

    before merge with origin master -- invoice finition

--- src/ContinuousNet/PubliPrBundle/Resources/public/js/components/Invoice/InvoiceCtrl.js
+++ src/ContinuousNet/PubliPrBundle/Resources/public/js/components/Invoice/InvoiceCtrl.js
@@ -20,6 +20,7 @@ app.controller('InvoiceCtrl',['$scope', '$rootScope','$stateParams', '$sce', '$t
             console.log(url);
             var params = {url : url, paymentId : paymentId};
             $invoiceDownloadFactory.download(params).$promise.then(function(data){
+console.log(data.fileName);
                 saveAs(data.blob, data.fileName);
                 deferred.resolve(data.fileName);
             });
